4a343ee73f8f4de9604b2d3592283d09dc4af1da
Godswill Enaohwo <godswilleo@yahoo.com>
2022-10-19 17:14:43 +0000

    _printf

--- _printf.c
+++ _printf.c
@@ -15,15 +15,19 @@ int _printf(const char *format, ...)
 	int (*pfunc)(va_list, flags_t *);
 	const char *p;
 	va_list args;
+
 	flags_t flags = {0, 0, 0};
 
 	register int count = 0;
 
 	va_start(args, format);
+
 	if ((format[0] == '%' && !format[1]) || !format)
 		return (-1);
+
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
+
 	for (p = format; *p; p++)
 	{
 		if (*p == '%')
@@ -43,7 +47,9 @@ int _printf(const char *format, ...)
 		} else
 			count += _putchar(*p);
 	}
+
 	_putchar(-1);
 	va_end(args);
 	return (count);
+
 }
