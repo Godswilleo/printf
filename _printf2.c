0e5604148e73a6d2dca1deeb4c425d69b526c3dd
Godswill Enaohwo <godswilleo@yahoo.com>
2022-10-21 05:31:59 +0000

    printf update

---
+++ _printf2.c
@@ -0,0 +1,55 @@
+#include "main.h"
+
+/**
+ * _printf - generates an output according to the format
+ * @format: format string containing the characters and the specifiers
+ * Description: calls the get_print() function and determine which
+ * what will be printed depending on what was specified
+ *
+ * Return: length of the string which has been formated
+ * Authors: Godswill Enaohwo & Oluwashogo Akadiri
+ */
+
+int _printf(const char *format, ...)
+{
+	int (*pfunc)(va_list, flags_t *);
+	const char *p;
+	va_list args;
+
+	flags_t flags = {0, 0, 0};
+
+	register int count = 0;
+
+	va_start(args, format);
+
+	if ((format[0] == '%' && !format[1]) || !format)
+		return (-1);
+
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+
+	for (p = format; *p; p++)
+	{
+		if (*p == '%')
+		{
+			p++;
+			if (*p == '%')
+			{
+				count += _putchar('%');
+				continue;
+			}
+			while (get_flag(*p, &flags))
+				p++;
+			pfunc = get_print(*p);
+			count += (pfunc)
+				? pfunc(args, &flags)
+				: _printf("%%%c", *p);
+		} else
+			count += _putchar(*p);
+	}
+
+	_putchar(-1);
+	va_end(args);
+	return (count);
+
+}
